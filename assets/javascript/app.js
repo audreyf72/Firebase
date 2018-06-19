bb3e7f284339ba2b02f12649ec14a7039792b601
Art Department | Screen Art <artdept@ArtDept-iMac.local>
2018-06-19 16:52:42 -0700

    Added javascript and firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,90 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDZP74h9QJRlGVjUdWQ0gIhbhE3AbH5t_8",
+    authDomain: "train-tracker-f8064.firebaseapp.com",
+    databaseURL: "https://train-tracker-f8064.firebaseio.com",
+    projectId: "train-tracker-f8064",
+    storageBucket: "",
+    messagingSenderId: "702358806553"
+  };
+  firebase.initializeApp(config);
+
+//Initialize document ready function
+$(document).ready(function() {  
+
+var database = firebase.database();
+var currentTime = moment();
+
+database.ref().on("child_added", function(childSnap) {
+
+    var name = childSnap.val().name;
+    var destination = childSnap.val().destination;
+    var firstTrain = childSnap.val().firstTrain;
+    var frequency = childSnap.val().frequency;
+    var min = childSnap.val().min;
+    var next = childSnap.val().next;
+
+    $("#trains > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
+});
+
+database.ref().on("value", function(snapshot) {
+   
+
+});
+
+//grabs information from the form
+$("#addTrain").on("click", function() {
+
+    var trainName = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrain = $("#firstTrain").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    //ensures that each input has a value
+    if (trainName == "") {
+        alert('Enter a train name.');
+        return false;
+    }
+    if (destination == "") {
+        alert('Enter a destination.');
+        return false;
+    }
+    if (firstTrain == "") {
+        alert('Enter a first train time.');
+        return false;
+    }
+    if (frequency == "") {
+        alert('Enter a frequency');
+        return false;
+    }
+
+    // THE MATH!
+    //subtracts the first train time back a year to ensure it's before current time.
+    var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
+    // the time difference between current time and the first train
+    var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+    var remainder = difference % frequency;
+    var minUntilTrain = frequency - remainder;
+    var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm a");
+
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        min: minUntilTrain,
+        next: nextTrain
+    }
+
+    console.log(newTrain);
+    database.ref().push(newTrain);
+
+    $("#trainName").val("");
+    $("#destination").val("");
+    $("#firstTrain").val("");
+    $("#frequency").val("");
+
+    return false;
+});
+
+});
